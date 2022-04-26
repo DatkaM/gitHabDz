e8959110fba22341fac6308e45bdb5a368db9222
zhusupmamatzhanov <32612854+zhusupmamatzhanov@users.noreply.github.com>
2022-04-26 13:49:07 +0600

    homework 10

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int a = 300;
+        sum(300);
+
+    }
+    public static void sum(int a){
         int counter1 = 0;
 
         for (int i = 1; i < a; i++) {
