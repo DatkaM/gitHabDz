bd1c2d4cedb4c22dd34d844a9102d694a6dfc19e
zhusupmamatzhanov <32612854+zhusupmamatzhanov@users.noreply.github.com>
2022-05-10 11:17:02 +0600

    homework 9

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,18 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        sum(300);
+        System.out.println("\n sum: " + sum(300));
 
     }
-    public static void sum(int a){
-        int counter1 = 0;
+    public static int sum(int a){
+        int counter = 0;
 
         for (int i = 1; i < a; i++) {
             if (i % 3 == 0 && i % 4 == 0 && i % 5 == 0) {
-                counter1 += i;
+                counter += i;
                 System.out.print( i + " ");
+
             }
         }
-        System.out.println("\n sum: " + counter1);
+        return  counter;
     }
 }
